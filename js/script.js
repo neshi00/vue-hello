a76524caf8604ffc5074aa2365ef836efab1b313
neshi00 <luca.ascione85@gmail.com>
2022-01-15 16:27:51 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -2,5 +2,13 @@ new Vue({
   el: "#app",
   data: {
     messaggio: "Hello Vue",
+    immagineRandom: "https://unsplash.it/300/300?image=",
+    input: " ",
+    h1Colors: "colors",
+  },
+  methods: {
+    inviaNome: function () {
+      this.messaggio = "Ciao" + this.input;
+    },
   },
 });
